08b7f054b0a49b4064966b897f62d7f1f2377454
shadowmoonezios <agushifelde@hotmail.com>
2025-08-15 06:44:30 +0300

    feat: initial commit for src/components/Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Urlaub Planer</h1>
+      <nav>
+        <ul>
+          <li>Start</li>
+          <li>Ziele</li>
+          <li>Buchungen</li>
+          <li>Reiserouten</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
