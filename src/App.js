77ef9ef42a1bb27b37a548c4f33e44da3f6834da
shadowmoonezios <agushifelde@hotmail.com>
2025-08-15 06:44:27 +0300

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Urlaub Planer App</h1>
+        <p>Willkommen zur Urlaub Planer App!</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
